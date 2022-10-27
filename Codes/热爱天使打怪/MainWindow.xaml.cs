1a93ebf30705017980ee5bf154d401facedb0ecc
Dai Li <d.li@simplylive.tv>
2022-10-27 14:28:43 +0800

    ff

--- Codes/热爱天使打怪/MainWindow.xaml.cs
+++ Codes/热爱天使打怪/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
@@ -18,6 +19,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Point = System.Drawing.Point;
 
 namespace 热爱天使打怪
 {
@@ -27,14 +29,22 @@ namespace 热爱天使打怪
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        System.Windows.Threading.DispatcherTimer updateTimer = new System.Windows.Threading.DispatcherTimer();
         Thread clickThread = null;
         bool m_Running = true;
         public MainWindow()
         {
             InitializeComponent();
             clickThread = new Thread(calltoClickThread);
-            clickThread.Start();
+           // clickThread.Start();
+            updateTimer.Tick += UpdateTimer_Tick;
+            updateTimer.Interval = new TimeSpan(0, 0, 1);
+            updateTimer.Start();
+        }
+
+        private void UpdateTimer_Tick(object? sender, EventArgs e)
+        {
+            onFindWindowBTN(sender, null);
         }
 
         private void onClosing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -56,17 +66,21 @@ namespace 热爱天使打怪
         Bitmap bitmap1=null, bitmap2;
 
         Int32 curPos = 0;
+       
         void takePhoto()
         {
             var image = WindowAPIHelper.GetShotCutImage(gameWidnow);
+            image = comparePicture.grayPicture(image); ;
             var bitmapImage = BitmapConveters.ConvertToBitmapImage(image);
             if (curPos++ % 2 == 0)
             {
                 myImage.Source = bitmapImage;
+                image.Save("c:\\logs\\111.png");
                 bitmap1 = image;
             }
             else
             {
+                image.Save("c:\\logs\\222.png");
                 myImage2.Source = bitmapImage;
                 bitmap2 = image;
             }
@@ -98,8 +112,40 @@ namespace 热爱天使打怪
                 lastX = mapItem.Key;
                 ia++;
             }
-            xText+= "aa"+newList.Count().ToString();
+            xText+= "aa"+ xList.Count().ToString();
             tbResult.Text = xText;
+            ia = 0;
+            xText = "";
+            foreach (var mapItem in newList)
+            {
+                xText += $"{ia}--{mapItem.Key}-> \n";
+                foreach(var item in mapItem.Value)
+                {
+                    xText += $"{item}  ";
+                }
+                xText += "\n";
+            }
+            tbResult2.Text = xText;
+            //System.Drawing.Imaging.PixelFormat pf = System.Drawing.Imaging.PixelFormat.Format1bppIndexed;//5+1+a+s+p+x
+            //BitmapData bmpData = imgObj.LockBits(new System.Drawing.Rectangle(0, 0, imgObj.Width, imgObj.Height), ImageLockMode.ReadWrite, pf);
+            //unsafe
+            //{
+            //    byte* ptr = (byte*)(bmpData.Scan0);
+            //    byte temp = 0;
+            //    for (int i = 0; i < bmpData.Height; i++)
+            //    {
+            //        for (int j = 0; j < bmpData.Width; j++)
+            //        {
+            //            temp = (byte)(0.229 * ptr[2] + 0.587 * ptr[1] + 0.144 * ptr[0]);
+            //            temp = (byte)(temp * 10);
+            //            ptr[0] = ptr[1] = ptr[2] = temp;
+            //            ptr += 3;
+            //        }
+            //        ptr += bmpData.Stride - bmpData.Width * 3;
+            //    }
+            //}
+            //imgObj.UnlockBits(bmpData);
+            return imgObj;
             foreach (var mapItem in vlist)
             {
                 foreach (var v in mapItem.Value)
@@ -125,6 +171,7 @@ namespace 热爱天使打怪
            
             return imgObj;
         }
+        ComparePicture comparePicture = new ComparePicture();
         SortedDictionary<int, List<System.Drawing.Rectangle>> rectangles = null;
         void compareTwoPic()
         {
@@ -135,10 +182,8 @@ namespace 热爱天使打怪
             var ah2 = myImage2.Source.Height;
 
 
-            ComparePicture cp = new ComparePicture();
 
-            rectangles = cp.compareTwo(bitmap1, bitmap2);
-
+            rectangles = comparePicture.compareTwo(bitmap1, bitmap2);
             myImage2.Source = BitmapConveters.ConvertToBitmapImage(changeColor(bitmap2, rectangles));
         }
         void findValidRect(List<System.Drawing.Rectangle> _rectangles)
@@ -173,10 +218,10 @@ namespace 热爱天使打怪
             }
         }
         private IntPtr hWndOriginalParent;
+
         private void onRBTN(object sender, RoutedEventArgs e)
         {
-            WindowAPIHelper.OnClickRButton(gameWidnow, 300, 300);
-
+            WindowAPIHelper.TTTT();
         }
 
         private void onsetParent(object sender, RoutedEventArgs e)
--- Codes/热爱天使打怪/WindowAPIHelper.cs
+++ Codes/热爱天使打怪/WindowAPIHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace 热爱天使打怪
 {
@@ -52,14 +54,60 @@ namespace 热爱天使打怪
         {
             return ((p_2 << 16) | (p & 0xFFFF));
         }
+        [DllImport("Shcore.dll")]
+        private static extern int GetProcessDpiAwareness(IntPtr hprocess, out PROCESS_DPI_AWARENESS value);
+
+        [DllImport("User32.dll")]
+        static extern IntPtr MonitorFromWindow(IntPtr hWnd, int dwFlags);
+        private enum PROCESS_DPI_AWARENESS
+        {
+            PROCESS_DPI_UNAWARE = 0,
+            PROCESS_SYSTEM_DPI_AWARE = 1,
+            PROCESS_PER_MONITOR_DPI_AWARE = 2
+        }
         public static void OnClickRButton(IntPtr hWnd, int x, int y)
         {
             int curPos = MAKELPARAM(x, y);
            // bool b1 = PostMessage(hWnd, (int)WMessages.WM_MOUSEMOVE, 0, curPos);
             System.Threading.Thread.Sleep(10);
-            PostMessage(hWnd, (int)WMessages.WM_RBUTTONDOWN, 0, curPos);
-            PostMessage(hWnd, (int)WMessages.WM_RBUTTONUP, 0, curPos);
+             PostMessage(hWnd, (int)WMessages.WM_RBUTTONDOWN, 0, curPos);
+             PostMessage(hWnd, (int)WMessages.WM_RBUTTONUP, 0, curPos);
+           
+        }
+        private const int MONITOR_DEFAULTTONULL = 0x00000000;
+        private const int MONITOR_DEFAULTTOPRIMARY = 0x00000001;
+        private const int MONITOR_DEFAULTTONEAREST = 0x00000002;
+        private const int MONITORINFOF_PRIMARY = 0x00000001;
+        public static void TTTT()
+        {
+            System.Diagnostics.Process proc = Process.GetProcessById(21464);
+            PROCESS_DPI_AWARENESS value;
+            int res = GetProcessDpiAwareness(proc.Handle, out value);
+            System.Diagnostics.Debug.Print(value.ToString());
 
+            // Get the monitor that the window is currently displayed on
+            // (where hWnd is a handle to the window of interest).
+          //  IntPtr hMonitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTONEAREST);
+
+            //// Get the logical width and height of the monitor.
+            //MONITORINFOEX miex;
+            //miex.cbSize = sizeof(miex);
+            //GetMonitorInfo(hMonitor, &miex);
+            //int cxLogical = (miex.rcMonitor.right - miex.rcMonitor.left);
+            //int cyLogical = (miex.rcMonitor.bottom - miex.rcMonitor.top);
+
+            //// Get the physical width and height of the monitor.
+            //DEVMODE dm;
+            //dm.dmSize = sizeof(dm);
+            //dm.dmDriverExtra = 0;
+            //EnumDisplaySettings(miex.szDevice, ENUM_CURRENT_SETTINGS, &dm);
+            //int cxPhysical = dm.dmPelsWidth;
+            //int cyPhysical = dm.dmPelsHeight;
+
+            //// Calculate the scaling factor.
+            //double horzScale = ((double)cxPhysical / (double)cxLogical);
+            //double vertScale = ((double)cyPhysical / (double)cyLogical);
+            //ASSERT(horzScale == vertScale);
         }
 
         public static void OnClickLButton(IntPtr hWnd, int x, int y)
